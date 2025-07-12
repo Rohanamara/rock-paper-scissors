2f928061c10594eaa09cf3a784ca049a4aebfefd
Amara Rohan Raj <123942731+Rohanamara@users.noreply.github.com>
2025-07-12 18:37:49 +0530

    Add playGame func

--- javascript.js
+++ javascript.js
@@ -5,7 +5,7 @@ function getComputerChoice(){
     return rps[choice];
 }
 
-const computer = getComputerChoice();
+// const computer = getComputerChoice();
 
 function getHumanChoice(){
     const choice = prompt("Choose rock/paper/scissor");
@@ -13,9 +13,50 @@ function getHumanChoice(){
     return choice;  
 }
 
-const human = getHumanChoice();
+// const human = getHumanChoice();
 
-const humanScore = 0;
-const computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
 
+function playRound(humanChoice, compChoice){
+    console.log(humanChoice,compChoice);
+    if ((humanChoice == "rock" && compChoice == "paper") || 
+        (humanChoice == "paper" && compChoice == "scissor") || 
+        (humanChoice == "scissor" && compChoice == "rock")  ){
+        console.log("You lose!");
+        computerScore++;
+    }else if ((humanChoice == "rock" && compChoice == "scissor")||
+            (humanChoice == "paper" && compChoice == "rock") ||
+            (humanChoice == "scissor" && compChoice == "paper")) {
+                console.log("You win!");
+                humanScore++;
+    }else if(humanChoice == compChoice){
+        console.log("It's a tie!");
+    }else{
+        console.log("Invalid choice. Please try again.");
+    }
+    console.log(humanScore,computerScore);
+
+
+}
+function playGame(){
+    for(i = 0; i < 5; i++){
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+        playRound(humanChoice,computerChoice);
+    }
+    if(humanScore > computerScore){
+        console.log("Human wins!");
+    }else if(humanScore < computerScore){
+        console.log("Computer wins!");
+    }else{
+        console.log("It's a tie!");
+    }
+}
+
+playGame();
+// 
+// playRound(human, computer)
+// 
+// console.log(humanScore, computerScore);
